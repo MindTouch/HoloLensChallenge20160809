a251dc1012d923180cda45cd23c9c714d8b90ed2
Steve Bjorg <steve.bjorg@gmail.com>
2016-08-03 18:03:08 -0700

    HoloLens .NET Magic Night challenge project with assets.

---
+++ Assets/Scripts/CursorScript.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class CursorScript : MonoBehaviour {
+
+    // Use this for initialization
+    public void Start() {
+    }
+
+    // Update is called once per frame
+    public void Update() {
+    }
+}
---
+++ Assets/Scripts/GazeGestureManager.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class GazeGestureManager : MonoBehaviour {
+
+    // Use this for initialization
+    public void Start() {
+    }
+
+    // Update is called once per frame
+    public void Update() {
+    }
+}
