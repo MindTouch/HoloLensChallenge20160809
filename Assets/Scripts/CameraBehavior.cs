f4e417c5743b97d868cd7fd2b13ba78c78d2388e
Steve Bjorg <steve.bjorg@gmail.com>
2016-08-04 10:53:40 -0700

    Renamed scripts and attached a script file to each object in the scene. This makes it easier to experiment with various behaviors on objects.

---
+++ Assets/Scripts/CameraBehavior.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class CameraBehavior : MonoBehaviour {
+
+    //--- Methods ---
+    public void Start() {
+
+        // TODO: use this for initialization
+    }
+
+    public void Update() {
+
+        // TODO: update is called once per frame
+    }
+}
---
+++ Assets/Scripts/CursorBehavior.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class CursorBehavior : MonoBehaviour {
+
+    //--- Methods ---
+    public void Start() {
+
+        // TODO: use this for initialization
+    }
+
+    public void Update() {
+
+        // TODO: update is called once per frame
+    }
+}
